6980dcad5f9b0d7829627412a0b5b3ea90f164d9
juyeongno1 <juyeongno1@naver.com>
2024-04-01 23:26:10 +0900

    set :: project setting

---
+++ src/main/kotlin/com/gbook/gbookbackend/GbookBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.gbook.gbookbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GbookBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<GbookBackendApplication>(*args)
+}
