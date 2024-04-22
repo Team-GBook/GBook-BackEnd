73c55a27feb1064f70549c62d3570690600b818f
김주영 <jyk102999@gmail.com>
2024-04-22 23:38:49 +0900

    merge :: 이메일 인증 리팩토링 (#6)
    * refactor :: validate
    
    * chore :: modify mail validation

--- src/main/kotlin/com/gbook/gbookbackend/domain/user/service/UserSignUpService.kt
+++ src/main/kotlin/com/gbook/gbookbackend/domain/user/service/UserSignUpService.kt
@@ -1,8 +1,11 @@
 package com.gbook.gbookbackend.domain.user.service
 
 import com.gbook.gbookbackend.domain.user.domain.User
+import com.gbook.gbookbackend.domain.user.domain.repository.MailRepository
 import com.gbook.gbookbackend.domain.user.domain.repository.UserRepository
 import com.gbook.gbookbackend.domain.user.exception.AlreadyUserException
+import com.gbook.gbookbackend.domain.user.exception.InvalidMailException
+import com.gbook.gbookbackend.domain.user.facade.MailFacade
 import com.gbook.gbookbackend.domain.user.presentation.dto.UserSignUpRequest
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
@@ -10,7 +13,9 @@ import org.springframework.stereotype.Service
 @Service
 class UserSignUpService(
     private val userRepository: UserRepository,
-    private val passwordEncoder: PasswordEncoder
+    private val passwordEncoder: PasswordEncoder,
+    private val mailRepository: MailRepository,
+    private val mailFacade: MailFacade,
 ) {
     fun execute(request: UserSignUpRequest) {
         validUser(request)
@@ -28,6 +33,10 @@ class UserSignUpService(
     private fun validUser(request: UserSignUpRequest) {
         if (userRepository.existsByEmail(request.email) || userRepository.existsByNickName(request.nickName)) {
             throw AlreadyUserException
+        }
+
+        if (!mailRepository.existsByEmail(request.email) || !mailFacade.getByMail(request.email).isChecked) {
+            throw InvalidMailException
         }
     }
 }
