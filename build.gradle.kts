66b99c2a6cf2c94b95f4fe3fcce60628f17d6bac
jyk1029 <jyk102999@gmail.com>
2024-05-01 11:56:50 +0900

    bug :: cd gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,6 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt:0.9.0")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     runtimeOnly("com.mysql:mysql-connector-j")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 }
 
