02ac829e4c5ae7583a7d3b18e5ca0340c030232e
jyk1029 <jyk102999@gmail.com>
2024-05-01 12:10:41 +0900

    bug :: cd gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,7 @@ plugins {
 
 group = "com.gbook"
 version = "0.0.1-SNAPSHOT"
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-}
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
