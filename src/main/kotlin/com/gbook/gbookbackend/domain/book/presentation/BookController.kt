4ed80c443a1484c13d41a975b46815ecbcb64d36
jyk1029 <jyk102999@gmail.com>
2024-05-03 09:03:02 +0900

    refactor :: like api

--- src/main/kotlin/com/gbook/gbookbackend/domain/book/presentation/BookController.kt
+++ src/main/kotlin/com/gbook/gbookbackend/domain/book/presentation/BookController.kt
@@ -1,12 +1,9 @@
 package com.gbook.gbookbackend.domain.book.presentation
 
-import com.gbook.gbookbackend.domain.book.service.CreateBookLikeService
-import com.gbook.gbookbackend.domain.book.service.DeleteBookLikeService
-import org.springframework.http.HttpStatus.CREATED
+import com.gbook.gbookbackend.domain.book.service.LikeBookService
 import org.springframework.http.HttpStatus.NO_CONTENT
-import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
@@ -14,18 +11,11 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/books")
 @RestController
 class BookController(
-    private val createBookLikeService: CreateBookLikeService,
-    private val deleteBookLikeService: DeleteBookLikeService,
+    private val likeBookService: LikeBookService,
 ) {
-    @ResponseStatus(CREATED)
-    @PostMapping("/like/{isbn}")
-    fun likeBook(@PathVariable isbn: String) {
-        createBookLikeService.execute(isbn)
-    }
-
     @ResponseStatus(NO_CONTENT)
-    @DeleteMapping("/dislike/{isbn}")
-    fun dislikeBook(@PathVariable isbn: String) {
-        deleteBookLikeService.execute(isbn)
+    @PutMapping("/like/{isbn}")
+    fun likeBook(@PathVariable isbn: String) {
+        likeBookService.execute(isbn)
     }
 }
