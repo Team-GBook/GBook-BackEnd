488a1c9c5a879713325cd430e06d6ebe19e569c6
jyk1029 <jyk102999@gmail.com>
2024-05-13 22:50:54 +0900

    bug :: internal server error

--- src/main/kotlin/com/gbook/gbookbackend/domain/review/service/DeleteReviewService.kt
+++ src/main/kotlin/com/gbook/gbookbackend/domain/review/service/DeleteReviewService.kt
@@ -1,6 +1,8 @@
 package com.gbook.gbookbackend.domain.review.service
 
+import com.gbook.gbookbackend.domain.review.domain.Review
 import com.gbook.gbookbackend.domain.review.domain.repository.ReviewRepository
+import com.gbook.gbookbackend.domain.review.exception.ReviewNotFoundException
 import com.gbook.gbookbackend.domain.user.facade.UserFacade
 import org.springframework.stereotype.Service
 
@@ -11,6 +13,11 @@ class DeleteReviewService(
 ) {
     fun execute(isbn: String) {
         val user = userFacade.getCurrentUser()
-        reviewRepository.deleteByIsbnAndUser(isbn, user)
+        val review: Review? = reviewRepository.findByIsbnAndUser(isbn, user)
+        if (review == null) {
+            throw ReviewNotFoundException
+        } else {
+            reviewRepository.delete(review)
+        }
     }
 }
